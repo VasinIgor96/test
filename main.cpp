329031460cfd9312ff98a94f60b47069ee55c473
student <student@PC-G-04>
2023-05-16 20:45:06 +0300

    пав

--- main.cpp
+++ main.cpp
@@ -4,41 +4,34 @@
 using namespace std;
 
 
-bool LogIn(fstream &file, string path, string login, string password)
+bool LogIn(File datafile, string path, string login, string password)
 {
-    string line, data = login + ' ' + password;
-    file.open(path, ios::in);
-    if (file.is_open())
+    string login, password;
+
+    string line, data = this->getLogin() + ' ' + this->getPassword();
+    datafile.open(path, ios::in);
+    if (datafile.is_open())
     {
         bool exitloop = true;
-        int counter = 0;
-        string buf;
-        while (getline(file, line, '\n') || exitloop)
+        while (getline(datafile, line, '\n') && exitloop)
         {
-            buf += line;
-            counter++;
-            cout << "Counter: " << counter << endl;
             if (line == data)
             {
-                cout << "If.." << endl;
-                file.close();
+                datafile.close();
+                cout << "You are in" << endl;
                 return true;
                 exitloop = false;
                 break;
             }
-            else
-            {
-                cout << "else.." << endl;                
-            }
         }
-        cout << buf << endl;
     }
     else
     {
         cout << "Error opening file" << endl;
         return false;
     }
-    file.close();
+    datafile.close();
+    cout << "Wrong login or password" << endl;
     return false;
 }
 
@@ -52,13 +45,14 @@ int main()
     cin >> login;
     cout << "Enter password: ";
     cin >> password;
-    if (LogIn(file, path, login, password))
+    LogIn(file, path, login, password);
+   /* if (LogIn(file, path, login, password))
     {
         cout << "You are in" << endl;
     }
     else
     {
         cout << "wrong login or password" << endl;
-    }
+    }*/
 	return 0;
 }
